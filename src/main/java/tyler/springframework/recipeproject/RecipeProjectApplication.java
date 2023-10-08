56c1b46234c27d3510927cffa66f5a74c5250a1e
Tyler Pearson <tyler.a.pear@gmail.com>
2023-10-08 15:06:43 -0500

    Adds Recipe, Ingredient, and Notes and defines JPA relationships. Closes #1

--- src/main/java/tyler/springframework/recipeproject/RecipeProjectApplication.java
+++ src/main/java/tyler/springframework/recipeproject/RecipeProjectApplication.java
@@ -1,13 +1,17 @@
 package tyler.springframework.recipeproject;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class RecipeProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RecipeProjectApplication.class, args);
+        System.out.println(SpringVersion.getVersion());
+        System.out.println(SpringBootVersion.getVersion());
     }
 
 }
---
+++ src/main/java/tyler/springframework/recipeproject/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package tyler.springframework.recipeproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        return "index";
+    }
+
+}
